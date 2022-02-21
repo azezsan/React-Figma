f69e2fc47aef599e2528ea9c3bfc11c14e1fdae4
TheShy <theshy.code@gmail.com>
2022-02-21 21:31:00 +0300

    Add Fonts

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,16 @@
 module.exports = {
-        mode: 'jit',
-        content: ['./pages/**/*.{js,ts,jsx,tsx}', './components/**/*.{js,ts,jsx,tsx}'],
-        theme: {
-                extend: {},
-        },
-        variants: {
-                display: ['group-hover'],
-        },
-        plugins: [],
+  mode: 'jit',
+  content: [
+    './pages/**/*.{js,ts,jsx,tsx}',
+    './components/**/*.{js,ts,jsx,tsx}',
+  ],
+  theme: {
+    extend: {
+      fontFamily: { open: ['Open Sans'], work: ['Work Sans'] },
+    },
+  },
+  variants: {
+    display: ['group-hover'],
+  },
+  plugins: [],
 };
